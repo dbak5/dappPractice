7ecb2fe6677c8e8f5fba15b1e0733083ffd4fb95
dbak5 <dahye.baker@gmail.com>
2024-01-24 15:17:15 +0800

    CC

--- contracts/Hostel.sol
+++ contracts/Hostel.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Hostel{
     address payable tenant;
--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,19 @@ module.exports = {
   networks: {
     loc_development_development: {
       network_id: "*",
-      port: 8545,
-      host: "127.0.0.1"
-    }
+      port: 7545,
+      host: "127.0.0.1",
+    },
+    loc_dappdevelopment_dappdevelopment: {
+      network_id: "*",
+      port: 7545,
+      host: "127.0.0.1",
+    },
   },
   mocha: {},
   compilers: {
     solc: {
-      version: "0.8.21"
-    }
-  }
+      version: "0.8.21",
+    },
+  },
 };
